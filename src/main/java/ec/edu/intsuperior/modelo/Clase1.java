cee081517f763ce86b646bd9dbdaf4a393ecff47
usuar <usuar@192.168.0.191>
2022-07-23 18:33:48 -0500

    Cjared_Prueba

---
+++ src/main/java/ec/edu/intsuperior/modelo/Clase1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author usuar
+ */
+public class Clase1 {
+    public static void metodoClase_1(){
+        System.out.println("metodo 1 de clase");
+    }
+    public static void metodoClase_2(){
+        System.out.println("metodo 2 de clase");
+    }
+     public static void metodoClase_3(){
+         System.out.println("metodo 3 de clase");
+    }
+     public static void metodoClase_4() {
+         System.out.println("metodo 4 de clase");
+    }
+     public static void metodoClase_5() {
+         System.out.println("metodo 5 de clase");
+    }
+
+    public void metodoInstancia_1() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
---
+++ src/main/java/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.modelo.Clase1;
+
+/**
+ *
+ * @author usuar
+ */
+public class Aplicacion {
+   public static void main(String[] args) {
+     Clase1 obj=new Clase1();
+     obj.metodoInstancia_1();
+   }
+}
+
+                
